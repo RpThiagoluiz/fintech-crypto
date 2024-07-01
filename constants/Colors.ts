9f36c78bd1d90bd5f1683d0ece0c25ad8b19504a
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2024-06-30 22:59:18 -0300

    chore: add new color

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -2,6 +2,7 @@ export default {
    primary: '#3D38ED',
    primaryMuted: '#C9C8FA',
    background: '#F5F5F5',
+   white: '#F0F0F0F0',
    dark: '#141518',
    gray: '#626D77',
    lightGray: '#D8DCE2',
