35cb448fbc9009995b64597daac2c4172a40438e
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2024-06-30 23:00:09 -0300

    feat: create authenticated tabs modals

---
+++ app/(authenticated)/(modals)/account.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+import { defaultStyles } from '@/constants/Styles';
+
+export const Page = () => (
+   <View style={defaultStyles.container}>
+      <Text style={defaultStyles.header}>Account PAGE</Text>
+   </View>
+);
+
+export default Page;
