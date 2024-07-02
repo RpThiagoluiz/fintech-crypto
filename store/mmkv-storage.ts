735114da081bf8d826665498dd988e1b620a8690
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2024-07-01 21:49:12 -0300

    feat: store local with zustang and mmkv

---
+++ store/mmkv-storage.ts
@@ -0,0 +1,15 @@
+import { MMKV } from 'react-native-mmkv';
+import { StateStorage } from 'zustand/middleware';
+
+const storage = new MMKV({
+   id: 'balance-storage',
+});
+
+export const zustandStorage: StateStorage = {
+   setItem: (name, value) => storage.set(name, value),
+   getItem: (name) => {
+      const value = storage.getString(name);
+      return value ?? null;
+   },
+   removeItem: (name) => storage.delete(name),
+};
